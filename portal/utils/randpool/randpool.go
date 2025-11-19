cbfb2e8286fa4d6d5d478183d9830d0667e21122
lemon-mint <ice.lemons.mint@gmail.com>
2025-11-19 10:59:11 +0900

    fix: ensure Rand overwrites buffer, fix fallback CSPRNG race condition, and add tests for overwrite and concurrency.

--- portal/utils/randpool/randpool.go
+++ portal/utils/randpool/randpool.go
@@ -9,18 +9,21 @@ import (
 	"golang.org/x/crypto/chacha20"
 )
 
-var _csprng_fallback = func() *chacha20.Cipher {
-	var initdata [12 + 32]byte // 12 byte nonce, 32 byte key
-	_, err := io.ReadFull(rand.Reader, initdata[:])
-	if err != nil {
-		panic(err)
-	}
-	c, err := chacha20.NewUnauthenticatedCipher(initdata[12:], initdata[:12])
-	if err != nil {
-		panic(err)
-	}
-	return c
-}()
+var (
+	_csprng_fallback_mu sync.Mutex
+	_csprng_fallback    = func() *chacha20.Cipher {
+		var initdata [12 + 32]byte // 12 byte nonce, 32 byte key
+		_, err := io.ReadFull(rand.Reader, initdata[:])
+		if err != nil {
+			panic(err)
+		}
+		c, err := chacha20.NewUnauthenticatedCipher(initdata[12:], initdata[:12])
+		if err != nil {
+			panic(err)
+		}
+		return c
+	}()
+)
 
 type chacha20rng struct {
 	c    *chacha20.Cipher
@@ -34,7 +37,9 @@ var _chacha20rngPool = sync.Pool{
 		if err != nil {
 			// if system rand fails, use fallback and print log
 			log.Println("randpool: chacha20rng init failed to read from system rand, using fallback")
+			_csprng_fallback_mu.Lock()
 			_csprng_fallback.XORKeyStream(initdata[:], initdata[:])
+			_csprng_fallback_mu.Unlock()
 		}
 		c, err := chacha20.NewUnauthenticatedCipher(initdata[12:], initdata[:12])
 		if err != nil {
@@ -53,6 +58,10 @@ func _chacha20rng() *chacha20rng {
 func chacha20rand(dst []byte) {
 	c := _chacha20rng()
 	c.used += uint64(len(dst))
+	// Zero out the destination buffer to ensure we overwrite instead of XOR
+	for i := range dst {
+		dst[i] = 0
+	}
 	c.c.XORKeyStream(dst, dst)
 	if c.used < 50*1<<30 {
 		// Return to pool only if we haven't used more than 50GiB
