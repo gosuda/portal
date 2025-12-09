f58b1e42dc098da0695fcf0d45cd02ea10bb63a4
cognitive-glitch <152830360+cognitive-glitch@users.noreply.github.com>
2025-12-09 12:55:50 +0900

    refactor(sdk): update for loop to range syntax

--- sdk/sdk_e2e_test.go
+++ sdk/sdk_e2e_test.go
@@ -277,7 +277,7 @@ func TestE2E_MultipleConnections(t *testing.T) {
 	numConnections := 5
 	log.Info().Int("count", numConnections).Msg("[TEST] Testing multiple concurrent connections")
 
-	for i := 0; i < numConnections; i++ {
+	for i := range numConnections {
 
 		go func() {
 			log.Debug().Int("conn_num", i).Msg("[TEST] Starting connection")
